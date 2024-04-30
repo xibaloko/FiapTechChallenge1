dcd73d8fd55cebf5152aa404e6d89b74e1a7ea23
Ulisses <ulissesmilitao@hotmail.com>
2024-04-29 21:11:38 -0300

    register controller implemented

--- FiapTechChallenge1/FiapTechChallenge.API/Controllers/RegisterController.cs
+++ FiapTechChallenge1/FiapTechChallenge.API/Controllers/RegisterController.cs
@@ -1,4 +1,5 @@
-﻿using FiapTechChallenge.Infra.Interfaces;
+﻿using FiapTechChallenge.Domain.Entities;
+using FiapTechChallenge.Infra.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,7 +16,7 @@ namespace FiapTechChallenge.API.Controllers
             _unitOfWork = unitOfWork;
         }
 
-        [HttpGet("contacts")]
+        [HttpGet("all-contacts")]
         public async Task<IActionResult> GetAllContacts()
         {
             var contacts = await _unitOfWork.Person.GetAllAsync();
@@ -23,12 +24,41 @@ namespace FiapTechChallenge.API.Controllers
             return Ok(contacts);
         }
 
-        [HttpGet("contacts/{id}")]
+        [HttpGet("contact-by-id/{id}")]
         public async Task<IActionResult> GetContactById(int id)
         {
             var contact = await _unitOfWork.Person.GetAllAsync(x => x.Id == id);
             // mudar pra response dto
             return Ok(contact);
+        }
+
+        [HttpGet("contact-by-region-id/{id}")]
+        public async Task<IActionResult> GetContactsByRegion(int id)
+        {
+            var contact = await _unitOfWork.Person.GetAllAsync(x => x.Id == id);
+            // mudar pra response dto
+            return Ok(contact);
+        }
+
+        [HttpPost("create-contact")]
+        public async Task<IActionResult> CreateContact([FromBody] Person person)
+        {
+
+            return Ok();
+        }
+
+        [HttpPut("update-contact/{id}")]
+        public async Task<IActionResult> UpdateContact(int id, [FromBody] Person person)
+        {
+           
+            return Ok();
+        }
+
+        [HttpDelete("delete-contact/{id}")]
+        public async Task<IActionResult> DeleteContact(int id)
+        {
+           
+            return Ok();
         }
     }
 }
