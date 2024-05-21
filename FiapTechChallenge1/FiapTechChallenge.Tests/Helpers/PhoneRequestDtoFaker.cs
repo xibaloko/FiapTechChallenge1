323e341559e9d4723f26b33fa8c1f9df837265b1
Heiter Malagoli <heiter.malagoli@newm.com.br>
2024-05-20 22:38:23 -0300

    Testes de DTOs feitos

---
+++ FiapTechChallenge1/FiapTechChallenge.Tests/Helpers/PhoneRequestDtoFaker.cs
@@ -0,0 +1,17 @@
+﻿using Bogus;
+using FiapTechChallenge.Domain.DTOs.RequestsDto;
+
+namespace FiapTechChallenge.Tests.Helpers
+{
+    public static class PhoneRequestDtoFaker
+    {
+        public static PhoneRequestByDDDDto GeneratePhoneRequest()
+        {
+            return new Faker<PhoneRequestByDDDDto>("pt_BR")
+                .RuleFor(p => p.PhoneNumber, f => f.Phone.PhoneNumber("##########"))
+                .RuleFor(p => p.DDDNumber, f => f.Random.Int(1, 99))
+                .RuleFor(p => p.PhoneType, f => f.PickRandom("Residencial", "Comercial", "Celular"))
+                .Generate();
+        }
+    }
+}
