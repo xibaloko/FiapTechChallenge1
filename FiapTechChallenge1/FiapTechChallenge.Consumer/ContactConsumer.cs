3d5b5be12e9507eaecba3e57d4f5622fc047c7f7
Raphael Muller <raphaelrmuller@gmail.com>
2024-08-27 22:23:14 -0300

    Refatoração e ajustes de usings e duplicações
    Removidas as linhas `using FiapTechChallenge.Producer.DTOs;` e
    `using FiapTechChallenge.AppService.Services;`. Adicionadas as linhas
    `using FiapTechChallenge.AppService.Interfaces;` e
    `using FiapTechChallenge.Domain.DTOs.RequestsDto;`. O namespace
    `FiapTechChallenge.Consumer` foi movido para baixo das novas declarações
    `using`.
    
    A classe `ContactConsumer` foi reformulada para remover duplicações na
    declaração da classe, do construtor e do método `Consume`.

--- FiapTechChallenge1/FiapTechChallenge.Consumer/ContactConsumer.cs
+++ FiapTechChallenge1/FiapTechChallenge.Consumer/ContactConsumer.cs
@@ -1,24 +1,23 @@
-﻿using FiapTechChallenge.Producer.DTOs;
-using MassTransit;
-using FiapTechChallenge.AppService.Services;
+﻿using MassTransit;
 using FiapTechChallenge.AppService.Interfaces;
-namespace FiapTechChallenge.Consumer
+using FiapTechChallenge.Domain.DTOs.RequestsDto;
+
+namespace FiapTechChallenge.Consumer;
+
+public class ContactConsumer : IConsumer<PersonRequestByDDDDto>
 {
-    public class ContactConsumer : IConsumer<PersonRequestByDDDDto>
+    private readonly IPersonService _personService;
+
+    public ContactConsumer(IPersonService personService)
     {
-        private readonly IPersonService _personService;
+        _personService = personService;
+    }
 
-        public ContactConsumer(IPersonService personService)
-        {
-            _personService = personService;
-        }
-
-        public async Task Consume(ConsumeContext<PersonRequestByDDDDto> context)
-        {
-            var contact = context.Message;
-            Console.WriteLine($"Contact received: {contact.Name} - {contact.Email}");
-            await _personService.CreateContact(contact);
-            return;
-        }
+    public async Task Consume(ConsumeContext<PersonRequestByDDDDto> context)
+    {
+        var contact = context.Message;
+        Console.WriteLine($"Contact received: {contact.Name} - {contact.Email}");
+        await _personService.CreateContact(contact);
+        return;
     }
 }
